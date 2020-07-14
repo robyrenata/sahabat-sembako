f0574579849c921872b8d11e943b5db2a8a0c96f
Roby Ibrahim Renata <roby.renata@codify.id>
2020-07-14 10:42:28 +0700

    product page add redirection

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, TemplateRef } from "@angular/core";
 import dataProduct from "./../../../assets/data/product-list.json";
 import { environment } from "src/environments/environment";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
 @Component({
   selector: "app-products",
   templateUrl: "./products.component.html",
@@ -8,11 +9,34 @@ import { environment } from "src/environments/environment";
 })
 export class ProductsComponent implements OnInit {
   productList = dataProduct;
-  constructor() {}
+  modalRef: BsModalRef;
+  constructor(private modalSrv: BsModalService) {}
 
   ngOnInit() {}
 
-  goToTokped() {
-    window.open(environment.url_tokped, "_BLANK");
+  openModalRedirect(template: TemplateRef<any>) {
+    this.modalRef = this.modalSrv.show(template, {
+      animated: true,
+      keyboard: true,
+      class: "modal-lg",
+    });
+  }
+
+  redirectPath(path) {
+    // window.open(environment.url_tokped + sanitized, "_BLANK");
+    let url = "";
+    switch (path) {
+      case "tokopedia":
+        url = environment.url_tokopedia;
+        break;
+      case "bukalapak":
+        url = environment.url_bukalapak;
+        break;
+      case "blibli":
+        url = environment.url_blibli;
+        break;
+    }
+    this.modalRef.hide();
+    window.open(url, "_BLANK");
   }
 }
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -4,6 +4,8 @@ import { ProductsComponent } from "./products.component";
 import { Routes, RouterModule } from "@angular/router";
 import { CardProductModule } from "src/app/shared/component/card-product/card-product.module";
 import { FooterModule } from "src/app/shared/component/footer/footer.module";
+import { SharedModule } from "src/app/shared/shared.module";
+import { ModalModule } from "ngx-bootstrap/modal";
 
 const routes: Routes = [
   {
@@ -19,6 +21,8 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     CardProductModule,
     FooterModule,
+    SharedModule,
+    ModalModule.forRoot(),
   ],
 })
 export class ProductsModule {}
